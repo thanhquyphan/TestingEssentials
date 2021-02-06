85dbee2fd000872339851cb75032ddccd1e1d429
thanhquyphan <thanhquyphan@live.com>
2021-02-06 20:07:06 +1100

    Added Testing Context, Json Mock Converter and Mock Extensions

---
+++ src/TQP.TestingEssentials/TQP.TestingEssentials.Core/JsonMockConverter.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace TQP.TestingEssentials.Core
+{
+    public class JsonMockConverter : JsonConverter
+    {
+        static readonly Dictionary<object, Func<object>> mockSerializers = new Dictionary<object, Func<object>>();
+        static readonly HashSet<Type> mockTypes = new HashSet<Type>();
+
+        public static void RegisterMock<T>(Mock<T> mock, Func<object> serializer) where T : class
+        {
+            mockSerializers[mock.Object] = serializer;
+            mockTypes.Add(mock.Object.GetType());
+        }
+
+        public override bool CanConvert(Type objectType) => mockTypes.Contains(objectType);
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) =>
+            throw new NotImplementedException();
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (!mockSerializers.TryGetValue(value, out var mockSerializer))
+            {
+                throw new InvalidOperationException("Attempt to serialize unregistered mock. Call '.RegisterForJsonSerialization()' after you do new Mock<T>()");
+            }
+            serializer.Serialize(writer, mockSerializer());
+        }
+    }
+}
